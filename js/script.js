6cf1784c3e824e1cf9beae8479ba53e353a81dfc
mrmrrr <14022216+mrmrrr@users.noreply.github.com>
2019-09-23 07:38:43 +0500

    1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 var feature = document.getElementById('feature');
+var newFont = document.getElementById('new');
+var firmFont = document.getElementById('firm');
 
 document.getElementById('svgBlend').addEventListener('mouseover',function(){
     document.getElementById('g1').style.transform='translateX(-50px)';
@@ -7,20 +9,46 @@ document.getElementById('svgBlend').addEventListener('mouseout',function(){
     document.getElementById('g1').style.transform='translateX(+50px)';
 })
 
-document.getElementById('svgBlendMobile').addEventListener('touchend',function(){
+document.getElementById('svgBlendMobile').addEventListener('touchstart',function(){
     document.getElementById('g1_').style.transform='translateY(+59px)';
 })
-// document.getElementById('svgBlendMobile').addEventListener('touchend',function(){
-//     document.getElementById('g1_').style.transform='translateY(0px)';
-// })
+document.getElementById('svgBlendMobile').addEventListener('touchend',function(){
+    document.getElementById('g1_').style.transform='translateY(0px)';
+})
 
+// ************************АНОНС
+document.getElementById('soon').addEventListener('mouseover',function(){
+    document.getElementById('soon').style.height='400vh';
+})
+newFont.addEventListener('mouseover', function(){
+    document.getElementById('gifNewFont').style.opacity='1';
+})
+newFont.addEventListener('mouseleave', function(){
+    document.getElementById('gifNewFont').style.opacity='0';
+})
 
-// document.querySelector('.fontTitle').addEventListener('mouseover',function(){
-//     document.getElementById('name-blend').style.opacity='1';
-// })
-// document.querySelector('.fontTitle').addEventListener('mouseout',function(){
-//     document.getElementById('name-blend').style.opacity='0';
-// })
+firmFont.addEventListener('mouseover',function(){
+    firmFont.setAttribute('src','img/newindex/firmHover.svg');
+})
+firmFont.addEventListener('mouseleave',function(){
+    firmFont.setAttribute('src','img/newindex/firm.svg');
+})
+
+    //mobile
+newFont.addEventListener('touchstart', function(){
+    document.getElementById('gifNewFont').style.opacity='1';
+})
+newFont.addEventListener('touchend', function(){
+    document.getElementById('gifNewFont').style.opacity='1';
+})
+firmFont.addEventListener('touchstart',function(){
+    firmFont.setAttribute('src','img/newindex/firmHover.svg');
+})
+firmFont.addEventListener('touchend',function(){
+    firmFont.setAttribute('src','img/newindex/firm.svg');
+})
+// ************************АНОНС_END
+
 
 document.getElementById('letters').addEventListener('mouseover',function(){
     document.getElementById('letters').style.opacity='1';
